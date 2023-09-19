995f5e48e0697fa799ccfd7bf47570b8adcf2399
Daniel Culpepper <culpepperd2490@gmail.com>
2023-09-19 16:51:31 -0400

    fs.mkdir for creating a folder

--- files.js
+++ files.js
@@ -12,15 +12,24 @@ const fs = require('fs');
 // console.log('last line');
 
 // writing files
-fs.writeFile('./docs/blog1.txt', 'Hello, from blog1.txt!', () => {
-    console.log('file was written');
-});
+// fs.writeFile('./docs/blog1.txt', 'Hello, from blog1.txt!', () => {
+//     console.log('file was written');
+// });
 
-fs.writeFile('./docs/blog2.txt', 'Hello, from blog2.txt!', () => {
-    console.log('file was written');
-});
+// fs.writeFile('./docs/blog2.txt', 'Hello, from blog2.txt!', () => {
+//     console.log('file was written');
+// });
 
 // directories
+if (!fs.existsSync('./assets')) {
+    fs.mkdir('./assets', (err) => {
+        if (err) {
+            console.log(err);
+            console.log('folder already exists');
+        }
+        console.log('folder created');
+    })
+}
 
 
 // deleting files

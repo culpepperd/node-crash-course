f37d90f70edbb61929add6824f4d7115146c3386
Daniel Culpepper <culpepperd2490@gmail.com>
2023-09-21 13:49:20 -0400

    using .pipe to read/write to a new file

--- streams.js
+++ streams.js
@@ -1,9 +1,18 @@
 const fs = require('fs');
 
-const readStream = fs.createReadStream('./docs/blog3.txt');
+// readStream is variable name
+// .createReadStream is ...
+// { encoding: 'utf8' } prints the data in a readable format
+const readStream = fs.createReadStream('./docs/blog3.txt', { encoding: 'utf8' });
 
-// .on is an event listener
-readStream.on('data', (chunk) => {
-    console.log('-!-!-!-!- NEW CHUNK -!-!-!-!-');
-    console.log(chunk.toString());
-})
+const writeStream = fs.createWriteStream('./docs/blog4.txt');
+
+// Event listener
+// readStream.on('data', (chunk) => {
+//     console.log('-!-!-!-!- NEW CHUNK -!-!-!-!-');
+//     console.log(chunk);
+//     writeStream.write('\nNEW CHUNK\n');
+//     writeStream.write(chunk);
+// })
+
+readStream.pipe(writeStream);

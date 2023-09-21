3879739a3e60db9898e9397d6c25b534dd56444b
Daniel Culpepper <culpepperd2490@gmail.com>
2023-09-21 13:35:34 -0400

    testing streaming data

--- streams.js
+++ streams.js
@@ -1,1 +1,9 @@
 const fs = require('fs');
+
+const readStream = fs.createReadStream('./docs/blog3.txt');
+
+// .on is an event listener
+readStream.on('data', (chunk) => {
+    console.log('-!-!-!-!- NEW CHUNK -!-!-!-!-');
+    console.log(chunk.toString());
+})

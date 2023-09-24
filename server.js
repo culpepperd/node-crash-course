490638847894ef0cfacff88555fde5ecf003af89
Daniel Culpepper <culpepperd2490@gmail.com>
2023-09-24 13:11:47 -0400

    added about.html

--- server.js
+++ server.js
@@ -1,4 +1,32 @@
 const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method);
+
+    // Set header content type
+    res.setHeader('Content-Type', 'text/html');
+
+    // Send an html file
+    fs.readFile('./views/index.html', (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            // if only sending one .write(), can put in .end()
+            // res.write(data);
+            res.end(data);
+        }
+    })
+});
+
+server.listen(3000, 'localhost', () => {
+    console.log('listening for requests on port 3000')
+});
+
+/*
+// ***** Simple Intro *****
+const http = require('http');
 
 // server variable set to the value of http.createServer
 // with a request and response object as parameters
@@ -20,3 +48,4 @@ const server = http.createServer((req, res) => {
 server.listen(3000, 'localhost', () => {
     console.log('listening for requests on port 3000')
 });
+*/

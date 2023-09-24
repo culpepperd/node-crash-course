aa28f5ab361098758ab5f2af3f95d095351d3d24
Daniel Culpepper <culpepperd2490@gmail.com>
2023-09-24 12:17:34 -0400

    testing request and response

--- server.js
+++ server.js
@@ -3,7 +3,18 @@ const http = require('http');
 // server variable set to the value of http.createServer
 // with a request and response object as parameters
 const server = http.createServer((req, res) => {
-    console.log(req);
+    console.log(req.url, req.method);
+
+    // 1. Set header content type
+    res.setHeader('Content-Type', 'text/html');
+
+    // 2. Write content to the browser
+    res.write('<head><link rel="stylesheet" href="#"></head>');
+    res.write('<p>Hello, World!</p>');
+    res.write('<p>Hello again, World!</p>');
+
+    // 3. End the response
+    res.end();
 });
 
 server.listen(3000, 'localhost', () => {

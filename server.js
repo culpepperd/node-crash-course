a459cd975114fff2f8d5cfc3af7e5b4c4588c871
Daniel Culpepper <culpepperd2490@gmail.com>
2023-09-23 23:38:40 -0400

    testing localhost:3000

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
 const http = require('http');
 
+// server variable set to the value of http.createServer
+// with a request and response object as parameters
+const server = http.createServer((req, res) => {
+    console.log(req);
+});
+
+server.listen(3000, 'localhost', () => {
+    console.log('listening for requests on port 3000')
+});

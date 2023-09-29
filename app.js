04cb68b697ed0d7f289d93f058abbf1a6c4122bd
Daniel Culpepper <culpepperd2490@gmail.com>
2023-09-29 15:43:12 -0400

    Using a path to direct response with a .html

--- app.js
+++ app.js
@@ -7,13 +7,12 @@ const app = express();
 app.listen(3000);
 
 app.get('/', (req, res) => {
-
-    res.send('<p>Hey look, World! No hands!</p>');
-
+    // res.send('<p>Hey look, World! No hands!</p>');
+    res.sendFile('./views/index.html', { root: __dirname });
 });
 
 app.get('/about', (req, res) => {
 
-    res.send('<p>Welcome to the About page!</p>');
+    // res.send('<p>Welcome to the About page!</p>');
 
 });

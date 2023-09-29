6153f0f9298f6c79d105bab05516f3835db4101d
Daniel Culpepper <culpepperd2490@gmail.com>
2023-09-29 15:37:05 -0400

    Using get request/response for About page

--- app.js
+++ app.js
@@ -11,3 +11,9 @@ app.get('/', (req, res) => {
     res.send('<p>Hey look, World! No hands!</p>');
 
 });
+
+app.get('/about', (req, res) => {
+
+    res.send('<p>Welcome to the About page!</p>');
+
+});

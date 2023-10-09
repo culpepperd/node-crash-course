dda70ee6133c6bddce9a95f19e7beb0f12f448ec
Daniel Culpepper <culpepperd2490@gmail.com>
2023-10-08 22:20:49 -0400

    added titles via ejs

--- app.js
+++ app.js
@@ -11,20 +11,20 @@ app.listen(3000);
 
 app.get('/', (req, res) => {
     // res.send('<p>Hey look, World! No hands!</p>');
-    res.render('index');
+    res.render('index', { title: 'Home' });
 });
 
 app.get('/about', (req, res) => {
     // res.send('<p>Welcome to the About page!</p>');
-    res.render('about');
+    res.render('about', { title: 'About Us' });
 });
 
 app.get('/blogs/create', (req, res) => {
-    res.render();
+    res.render('create', { title: 'Creat a new Blog' });
 })
 
 // 404 page - this will fire for anything NOT matching
 // the above URLs
 app.use((req, res) => {
-    res.status(404).render('404');
+    res.status(404).render('404', { title: '404' });
 });

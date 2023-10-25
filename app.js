556ec39ef4aab6bdf0c2cf3412fbed09ca6b56cf
Daniel Culpepper <culpepperd2490@gmail.com>
2023-10-25 11:58:27 -0400

    starting section 8 - middleware

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ app.set('view engine', 'ejs');
 // listen for requests
 app.listen(3000);
 
+// middleware
+app.use((req, res, next) => {
+    console.log('new request made:');
+    console.log('host: ', req.hostname);
+    console.log('path: ', req.path);
+    console.log('method: ', req.method);
+    next();
+});
+
 app.get('/', (req, res) => {
     const blogs = [
         {title: 'Yoshi finds eggs', snippet: 'Blarp Bleep Bloop'},
